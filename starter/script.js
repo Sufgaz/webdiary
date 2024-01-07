e1c9bcb11bf3e30f22f42b8191bd47a3157bdb6c
sufgaz <sufgaz911@gmail.com>
2024-01-07 17:09:33 +0000

    date and time added on JS using day js

---
+++ starter/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // Function to display current date and time
+  function displayDateTime() {
+    var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    var currentTime = dayjs().format('h:mm:ss A');
+    
+    var dateTimeString = currentDate + ' ' + currentTime;
+    $('#currentDay').text(dateTimeString);
+  }
+  // Initial call to display date and time
+  displayDateTime();
+  // Update date and time every minute
+  setInterval(displayDateTime, 1); // Update every second
+});
